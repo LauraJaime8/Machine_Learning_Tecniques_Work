1b8912387c6cdd52913e74ca2238f49d384a4584
Diego Andérica <dar-96@hotmail.com>
2017-10-18 18:36:24 +0200

    Activity 3 added

---
+++ Activity 3/activity3.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 15 20:01:48 2017
+
+@author Diego Andérica Richard, Ruth Rodríguez-Manzaneque López, Laura Jaime Villamayor
+
+"""
+import loaddata
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+from sklearn import metrics
+
+def plotdata(data,labels,name): #def function plotdata
+    fig, ax = plt.subplots()
+    plt.scatter([row[0] for row in data], [row[1] for row in data], c = labels)
+    ax.grid(True)
+    fig.tight_layout()
+    plt.title(name)
+    plt.show()
+
+# 0. Load Data
+records, names = loaddata.load_data("dengue_features_train.csv")
+
+# 1. Normalization of the data
+min_max_scaler = preprocessing.MinMaxScaler()
+records = min_max_scaler.fit_transform(records)
+
+# 2. PCA Estimation
+estimator = PCA (n_components = 2)
+X_pca = estimator.fit_transform(records)
+
+print(estimator.explained_variance_ratio_) 
+
+# 3. Plotting the data 
+labels = [0 for x in range(len(X_pca))]
+plotdata(X_pca, labels, 'Initial')
+
+# 4. Setting parameters (ad-hoc)
+# 4.1. Prameters
+init = 'k-means++' # initialization method 
+iterations = 10 # to run 10 times with different random centroids to choose the final model as the one with the lowest SSE
+max_iter = 300 # maximum number of iterations for each single run
+tol = 1e-04 # controls the tolerance with regard to the changes in the within-cluster sum-squared-error to declare convergence
+random_state = 0 # random
+
+silhouettes = []
+
+for i in range(2, 14):
+    km = KMeans(i, init, n_init = iterations ,max_iter= max_iter, tol = tol,random_state = random_state)
+    labels = km.fit_predict(X_pca)
+    silhouettes.append(metrics.silhouette_score(X_pca, labels))
+    
+# 4.2. Plot Silhouette
+plt.plot(range(2,14), silhouettes , marker='o')
+plt.xlabel('Number of clusters')
+plt.ylabel('Silhouette')
+plt.show()
+
+print "Due to the fact that 2 has the higher silhouette, this value will be used to set the value of k\n"
+
+# 5. Setting k value
+k = 2
+
+# 6. Clustering execution
+km = KMeans(k, init, n_init = iterations, max_iter= max_iter, tol = tol,random_state = random_state)
+labels = km.fit_predict(X_pca)
+
+# 7. Plotting final results
+plotdata(X_pca, labels, init)
+
+
+
---
+++ Activity 3/loaddata.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 14 12:05:59 2016
+
+@author Diego Andérica Richard, Ruth Rodríguez-Manzaneque López, Laura Jaime Villamayor
+
+"""
+import codecs
+def load_data(path):
+        
+    f = codecs.open(path, "r", "utf-8")
+    records = []
+    names = []
+    years = ["1997", "1998", "1999", "2000", "2001", "2002", "2003"]
+    first_row = True
+    n_elements = 0
+    
+    for line in f:
+        
+        if first_row == True:
+            row = line.split(",")
+            
+            for i in range(4, len(row)):
+                names.append(row[i])
+                
+            first_row = False
+        else:
+            #Replace no-data fields
+            while ",," in line:
+                line = line.replace(",,", ",0,")
+        
+            #Replace last unfilled field
+            line = line.replace(",\n", ",0\n")
+           
+            row = line.split(",")
+            
+            if row[0] == "sj" and row[1] in years:
+                for i in range (4):
+                    row.pop(0)
+            
+                if row != []:
+                    records.append(map(float, row))
+                    n_elements += 1
+
+    # Remove outliers
+    records.pop(52)
+    records.pop(104)
+    records.pop(358)
+    records.pop(103)
+    records.pop(356)
+	
+    print ""
+    print "Total number of elements: " + str(n_elements)
+    
+    return records, names
